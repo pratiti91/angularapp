75f1b1dda145603ea651732247ee8f1af8920624
codexco-007 <codesco007@gmail.com>
2022-07-30 22:34:56 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  ename="Pratiti Bhattacharjee";
+  project="Spring Boot";
+  projectUI="Angular 8"
+  display(){
+    alert("Welcome to angular");
+  }
+  uname=new FormControl();
+  updateName(){
+    this.uname.setValue("Pratiti");
+  }
+  p:number=0;
+  s:number=0;
+  jtall=false;
+  p_height=new FormControl(0);
+  s_height=new FormControl(0);
+  findTall(){
+  this.p=Number(this.p_height.value);
+  this.s=Number(this.s_height.value);
+  if(this.p>this.s)
+  this.jtall=true;
+  else
+  this.jtall=false;
+  alert("Function is called")
 }
+
+}
+
